9ea4c1aa0b3d8e6e45bc5833a7766a0458c6bdfb
bijbeher <bijendra.behera@oracle.com>
2019-05-18 02:28:17 +0530

    Cors Policy Support

--- src/main/java/com/config/CustomFilter.java
+++ src/main/java/com/config/CustomFilter.java
@@ -2,35 +2,40 @@ package com.config;
 
 import java.io.IOException;
 
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
+import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
-import org.springframework.stereotype.Component;
-import org.springframework.web.filter.OncePerRequestFilter;
-@Component
+//import org.springframework.stereotype.Component;
 @Configuration
 @Order(Ordered.HIGHEST_PRECEDENCE)
-public class CustomFilter extends OncePerRequestFilter {
+public class CustomFilter implements Filter  {
 
-    @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+    public  void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
             throws ServletException, IOException {
+        HttpServletResponse response = (HttpServletResponse) res;
+        HttpServletRequest request = (HttpServletRequest) req;
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers",
-                "authorization, content-type, xsrf-token, Cache-Control, remember-me, WWW-Authenticate");
+                "authorization, origin,content-type, xsrf-token, Cache-Control, remember-me, WWW-Authenticate,accept");
         response.addHeader("Access-Control-Expose-Headers", "xsrf-token");
+        if (request.getMethod().equals("OPTIONS")) {
+            response.setStatus(HttpServletResponse.SC_ACCEPTED);
+            return;
+        }
         chain.doFilter(request, response);
-
+        }
+    public void init(FilterConfig filterConfig) {
+        // not needed
     }
 
-    public void destroy() {}
-
+    public void destroy() {
+        //not needed
+    }
 
 }
