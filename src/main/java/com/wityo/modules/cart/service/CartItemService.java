3df6f42e14641b4ef45515012a8a3eef86f6ae2f
akash-beura <46479601+akash-beura@users.noreply.github.com>
2019-07-20 01:47:37 +0530

    Revert "CartItem module complete."
    This reverts commit d6dc99e8440c5e51048eadd883f843f174f8b1d5.

--- src/main/java/com/wityo/modules/cart/service/CartItemService.java
+++ src/main/java/com/wityo/modules/cart/service/CartItemService.java
@@ -1,11 +1,5 @@
 package com.wityo.modules.cart.service;
 
-import com.wityo.modules.cart.model.Cart;
-
 public interface CartItemService {
-	
-	public String addOrUpdateCart(Long productId, String quantityOptions);
-	public String deleteCartItemById(Long cartItemId);
-	public String removeAllCartItems(Cart cart);
-	public String reduceCartItem(Long productId, String quantityOption);
+
 }
