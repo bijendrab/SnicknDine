93c8a6c19db8c1b86545c0cff153c08e8d6e4918
akash-beura <46479601+akash-beura@users.noreply.github.com>
2019-07-20 16:10:16 +0530

    Revert "Revert "CartItem module complete.""
    This reverts commit 3df6f42e14641b4ef45515012a8a3eef86f6ae2f.

--- src/main/java/com/wityo/modules/cart/controller/CartItemController.java
+++ src/main/java/com/wityo/modules/cart/controller/CartItemController.java
@@ -1,29 +1,87 @@
 package com.wityo.modules.cart.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.wityo.modules.cart.service.CartItemService;
-import com.wityo.modules.cart.service.CartService;
-import com.wityo.modules.user.service.CustomerService;
+import com.wityo.modules.user.model.User;
 
 @RestController
 @CrossOrigin("*")
-@RequestMapping("/api/cart")
+@RequestMapping("/api/cartitem")
 public class CartItemController {
-	
-	@Autowired
-	private CartService cartService;
 	
 	@Autowired
 	private CartItemService cartItemService;		
 
-	@Autowired
-	private CustomerService customerService;
+	
+	/*
+	 * @Description: Function to add/update CartItem in user's cart.
+	 * 
+	 * */
+	@PostMapping("/addupdate/{productId}/{quantityOption}")
+	public ResponseEntity<?> addOrUpdateCartItem(@PathVariable String productId, @PathVariable String quantityOption){
+		Map<String, Object> response = new HashMap<String, Object>();
+		response.put("message", cartItemService.addOrUpdateCart(Long.parseLong(productId),quantityOption));
+		response.put("body", "");
+		response.put("status", HttpStatus.ACCEPTED);
+		response.put("error", false);
+		return new ResponseEntity<Map<String, Object>>(response, HttpStatus.ACCEPTED);
+	}
 
+	/*
+	 * @Description: Function to remove CartItem completely from user's cart
+	 * 
+	 * */
+	@DeleteMapping("/delete/{cartItemId}")
+	public ResponseEntity<?> removeCartItem(@PathVariable String cartItemId){
+		Map<String, Object> response = new HashMap<String, Object>();
+		response.put("message", cartItemService.deleteCartItemById(Long.parseLong(cartItemId)));
+		response.put("body", "");
+		response.put("status", HttpStatus.ACCEPTED);
+		response.put("error", false);
+		return new ResponseEntity<Map<String, Object>>(response, HttpStatus.ACCEPTED);
+	}
 	
 	
+	/*
+	 * @Description: Function to remove all CartItems from user's cart
+	 * 
+	 * */
+	@DeleteMapping("/delete/{cartItemId}")
+	public ResponseEntity<?> removeAllCartItems(){
+		User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		Map<String, Object> response = new HashMap<String, Object>();
+		response.put("message", cartItemService.removeAllCartItems(user.getCustomer().getCart()));
+		response.put("body", "");
+		response.put("status", HttpStatus.ACCEPTED);
+		response.put("error", false);
+		return new ResponseEntity<Map<String, Object>>(response, HttpStatus.ACCEPTED);
+	}
+	
+	
+	/*
+	 * @Description: Function to Decrement or Remove cartItem from cart
+	 * 
+	 * */
+	@DeleteMapping("/decrement/{productId}/{quantityOption}")
+	public ResponseEntity<?> decrementCartItemQuantity(@PathVariable Long productId, @PathVariable String quantityOption){
+		Map<String, Object> response = new HashMap<String, Object>();
+		response.put("message", cartItemService.reduceCartItem(productId, quantityOption));
+		response.put("body", "");
+		response.put("status", HttpStatus.ACCEPTED);
+		response.put("error", false);
+		return new ResponseEntity<Map<String, Object>>(response, HttpStatus.ACCEPTED);
+	}
 }
--- src/main/java/com/wityo/modules/cart/service/impl/CartItemServiceImpl.java
+++ src/main/java/com/wityo/modules/cart/service/impl/CartItemServiceImpl.java
@@ -2,6 +2,7 @@ package com.wityo.modules.cart.service.impl;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -10,37 +11,39 @@ import org.springframework.stereotype.Service;
 import com.wityo.modules.cart.model.Cart;
 import com.wityo.modules.cart.model.CartItem;
 import com.wityo.modules.cart.repository.CartItemRepository;
+import com.wityo.modules.cart.service.CartItemService;
 import com.wityo.modules.product.model.Product;
+import com.wityo.modules.product.model.ProductQuantityOption;
 import com.wityo.modules.product.repository.ProductRepository;
 import com.wityo.modules.user.model.Customer;
 import com.wityo.modules.user.model.User;
 
 @Service
-public class CartItemServiceImpl {
-	
+public class CartItemServiceImpl implements CartItemService{
+
 	@Autowired
 	CartItemRepository cartItemRepository;
-	
+
 	@Autowired
 	ProductRepository productRepository;
-	
+
 	public String addOrUpdateCart(Long productId, String quantityOptions) {
 		try {
-			User userDetail = (User)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+			User userDetail = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 			Customer customer = userDetail.getCustomer();
 			Cart cart = customer.getCart();
 			Product product = productRepository.findByProductId(productId);
-			
+
 			List<CartItem> userCartItems = cart.getCartItems();
 
-			if(userCartItems.size() == 0) {
+			if (userCartItems.size() == 0) {
 				userCartItems = new ArrayList<CartItem>();
 				CartItem newCartItem = new CartItem();
 				newCartItem.setCart(cart);
 				newCartItem.setItemName(product.getProductName());
 				newCartItem.setQuantity(1);
 				product.getProductQuantityOptions().forEach(qOption -> {
-					if(qOption.getQuantityOption().equalsIgnoreCase(quantityOptions)) {
+					if (qOption.getQuantityOption().equalsIgnoreCase(quantityOptions)) {
 						newCartItem.setPrice(qOption.getPrice() * 1);
 						newCartItem.setQuantityOption(quantityOptions);
 					}
@@ -48,52 +51,81 @@ public class CartItemServiceImpl {
 				cartItemRepository.save(newCartItem);
 				return "added";
 			} else {
-				userCartItems.parallelStream().forEach(cartItem -> {
-					if(productId == cartItem.getProduct().getProductId()) {
-						product.getProductQuantityOptions().parallelStream().forEach(qOption->{
-							if(qOption.getQuantityOption().equalsIgnoreCase(quantityOptions)
-								&& cartItem.getQuantityOption().equalsIgnoreCase(quantityOptions)) {
-								cartItem.setQuantity(cartItem.getQuantity()+1);
-								cartItem.setPrice(cartItem.getQuantity() * qOption.getPrice());
-								cartItemRepository.save(cartItem);
-							}
-							if(!qOption.getQuantityOption().equalsIgnoreCase(quantityOptions)
-									&& cartItem.getQuantityOption().equalsIgnoreCase(quantityOptions)) {
-								cartItem.setPrice(cartItem.getQuantity() * qOption.getPrice());
-								cartItem.setQuantityOption(qOption.getQuantityOption());
-								cartItemRepository.save(cartItem);
-							}
-						});
+				// Getting the cartItem which contains the product in the user's cartItems
+				// object
+				CartItem tempCartItem = userCartItems.parallelStream()
+						.filter(cartItem -> productId == cartItem.getProduct().getProductId()).findFirst().orElse(null);
+
+				if (tempCartItem != null) {
+					Product tempProduct = tempCartItem.getProduct();
+					Set<ProductQuantityOption> productQuantityOptions = tempProduct.getProductQuantityOptions();
+					for (ProductQuantityOption qOption : productQuantityOptions) {
+						if (qOption.getQuantityOption().equalsIgnoreCase(quantityOptions)
+								&& tempCartItem.getQuantityOption().equalsIgnoreCase(quantityOptions)) {
+							tempCartItem.setQuantity(tempCartItem.getQuantity() + 1);
+							tempCartItem.setPrice(tempCartItem.getQuantity() * qOption.getPrice());
+							cartItemRepository.save(tempCartItem);
+							break;
+						}
+						if (!qOption.getQuantityOption().equalsIgnoreCase(quantityOptions)
+								&& tempCartItem.getQuantityOption().equalsIgnoreCase(quantityOptions)) {
+							tempCartItem.setPrice(tempCartItem.getQuantity() * qOption.getPrice());
+							tempCartItem.setQuantityOption(qOption.getQuantityOption());
+							cartItemRepository.save(tempCartItem);
+							break;
+						}
 					}
-				});
+				}
 				return "updated";
 			}
-		}catch (Exception e) {
+		} catch (Exception e) {
 		}
 		return "error";
-		
+
 	}
-	
+
 	public String deleteCartItemById(Long cartItemId) {
 		try {
 			cartItemRepository.deleteById(cartItemId);
-			return "deleted";	
-		}catch (Exception e) {
-			System.out.println(e.getMessage());
-		}
-		return null;
-	}
-	
-	public String removeAllCartItems(Cart cart) {
-		try {
-			cart.getCartItems()
-			.parallelStream()
-			.forEach(cartItem -> cartItemRepository.deleteById(cartItem.getCartItemId()));
-			return "all items deleted";
+			return "deleted";
 		} catch (Exception e) {
-			// TODO: handle exception
 		}
-		return null;
+		return "failure";
 	}
 
+	public String removeAllCartItems(Cart cart) {
+		try {
+			cart.getCartItems().parallelStream()
+					.forEach(cartItem -> cartItemRepository.deleteById(cartItem.getCartItemId()));
+			return "all items deleted";
+		} catch (Exception e) {
+		}
+		return "failure";
+	}
+	
+	public String reduceCartItem(Long productId, String quantityOption){
+		User userDetail = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		Customer customer = userDetail.getCustomer();
+		Cart cart = customer.getCart();
+		for(CartItem cartItem : cart.getCartItems()) {
+			if(productId == cartItem.getProduct().getProductId()) {
+				int updatedQuantity = cartItem.getQuantity() - 1;
+				if(updatedQuantity == 0) {
+					cartItemRepository.deleteById(cartItem.getCartItemId());
+					return "cart updated";
+				}
+				cartItem.setQuantity(updatedQuantity);
+				for(ProductQuantityOption qOption : cartItem.getProduct().getProductQuantityOptions()) {
+					if(qOption.getQuantityOption().equalsIgnoreCase(quantityOption) 
+							&& cartItem.getQuantityOption().equalsIgnoreCase(quantityOption)) {
+						cartItem.setPrice(updatedQuantity * qOption.getPrice());
+						cartItemRepository.save(cartItem);
+						return "cart updated";
+					}
+				}
+				
+			}
+		}
+		return "operation failed";
+	}
 }
