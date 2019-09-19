bf74043fc8f89b7d20f625fdcb64f5e188587275
bijendrab <msjlitup@gmail.com>
2019-09-20 04:58:07 +0530

    Testfile name change for build process

--- src/test/java/com/wityo/wityo/WityoTests.java
+++ src/test/java/com/wityo/wityo/WityoTests.java
@@ -1,4 +1,4 @@
-package com.snickndine;
+package com.wityo.wityo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SnickNDineApplicationTests {
+public class WityoTests {
 
 	@Test
 	public void contextLoads() {
