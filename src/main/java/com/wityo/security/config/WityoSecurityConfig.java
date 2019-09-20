dfbcc69ded34a8ed1e337b2858f28f79c534d0e2
bijendrab <msjlitup@gmail.com>
2019-09-20 05:59:09 +0530

    swagger for wityouser. not working..need to check

--- src/main/java/com/wityo/security/config/WityoSecurityConfig.java
+++ src/main/java/com/wityo/security/config/WityoSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -35,7 +36,17 @@ public class WityoSecurityConfig extends WebSecurityConfigurerAdapter{
 			.anyRequest().authenticated();
 		http.addFilterBefore(authenticationFilter(), UsernamePasswordAuthenticationFilter.class);
 	}
-	
+
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/v2/api-docs",
+			"/configuration/ui",
+			"/swagger-resources/**",
+			"/configuration/security",
+			"/swagger-ui.html",
+			"/webjars/**");
+	}
+
 	@Bean
 	public JwtAuthenticationFilter authenticationFilter() {
 		return new JwtAuthenticationFilter();
